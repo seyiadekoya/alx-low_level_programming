ae7797cc67f135bd0fc5200acbd82df8310007c1
Seyi Adekoya <seyiadekoya29@gmail.com>
2022-10-23 09:13:27 -0700

    Adder

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - The sum of 2 integers
+* @a: First variable
+* @b: Second variable
+*
+* Return: the sum of the two variables
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
