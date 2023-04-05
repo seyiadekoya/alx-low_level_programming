822a568c9ecd1dfa1713c23bd7ca143ea37816aa
Seyi Adekoya <seyiadekoya29@gmail.com>
2023-04-04 20:25:37 -0700

    Readme and dog.h file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This project explores the concept of structures in C with various projects.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - This is a user defined structure that has 3 members
+* @name: First member
+* @age: Second member
+* @owner: Third member
+*
+* Description - This structure stores data of the name, age & owner of a dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
