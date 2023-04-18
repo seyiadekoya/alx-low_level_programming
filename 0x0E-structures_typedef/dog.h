5908961dd6dd0ca426f2e0953bbfd7e759c93b64
Seyi Adekoya <seyiadekoya29@gmail.com>
2023-04-18 15:56:33 -0700

    Updated the header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,12 @@
 * Description - This structure stores data of the name, age & owner of a dog
 */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
