a70e9dc6cc1ffaded0005bcaa099c596025bb81a
Seyi Adekoya <seyiadekoya29@gmail.com>
2023-04-18 15:42:51 -0700

    Fixed null pointer exception

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,16 +9,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name != NULL)
-		printf("Name: %s\n", d->name);
-	else
-		printf("Name: (nil)\n");
-	if (d->age <= 0)
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %f\n", d->age);
-	if (d->owner != NULL)
-		printf("Owner: %s\n", d->owner);
-	else
-		printf("Owner: (nil)\n");
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
