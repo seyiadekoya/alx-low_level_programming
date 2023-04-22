a5f741fa64e4b82e9b3b70a203a6103ace24fb7f
Seyi Adekoya <seyiadekoya29@gmail.com>
2023-04-22 10:41:51 -0700

    Fixed function errors

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 /**
  * new_dog - This function creates a struct & copies the name & owner members
@@ -11,37 +11,39 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *new_dog2;
+
 	/*condition to ensure the program returns NULL if null is passed*/
 	if (name == NULL || age == 0 || owner == NULL)
 		return (NULL);
 	/*Allocating memory for the new_dog structure*/
-	dog_t *new_dog = malloc(sizeof(dog_t));
+	new_dog2 = malloc(sizeof(dog_t));
 	/*condition checking if malloc failed to free the allocated memory*/
-	if (new_dog == NULL)
+	if (new_dog2 == NULL)
 	{
 		return (NULL);
 	}
 	/*Allocating memory to store the copy of name member in new_dog struct*/
-	new_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	new_dog2->name = malloc(sizeof(char) * (strlen(name) + 1));
 	/*condition checking if malloc failed to free the allocated memory*/
-	if (new_dog->name == NULL)
+	if (new_dog2->name == NULL)
 	{
-		free(new_dog);
+		free(new_dog2);
 		return (NULL);
 	}
 	/*copying the name value to the allocated memory*/
-	strcpy(new_dog->name, name);
-	new_dog->age = age;
+	strcpy(new_dog2->name, name);
+	new_dog2->age = age;
 	/*Allocating memory to store the copy of owner member in new_dog struct*/
-	new_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	new_dog2->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 	/*condition checking if malloc failed to free the allocated memory*/
-	if (new_dog->owner == NULL)
+	if (new_dog2->owner == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog);
+		free(new_dog2->name);
+		free(new_dog2);
 		return (NULL);
 	}
 	/*copying the owner value to the allocated memory*/
-	strcpy(new_dog->owner, owner);
-	return (new_dog);
+	strcpy(new_dog2->owner, owner);
+	return (new_dog2);
 }
